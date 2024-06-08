53c0e88b48e937670e40da685b3319165eec85b7
João Vitor Retamero <joaovretamero@gmail.com>
2024-06-08 11:05:03 -0300

    Added Swagger and routes with fake database

--- Program.cs
+++ Program.cs
@@ -1,8 +1,33 @@
+using Microsoft.OpenApi.Models;
+using PizzaStore.DB;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+    {
+        var apiVersion = "v1";
+
+        c.SwaggerDoc(apiVersion, new OpenApiInfo
+        {
+            Title = "Pizza Store API",
+            Description = "Making the Pizzas you love",
+            Version = apiVersion
+        });
+    });
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
+app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
 
 app.Run();
